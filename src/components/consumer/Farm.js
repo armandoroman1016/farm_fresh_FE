5b4b43c62a1e7808af8c23143465a19dc8f222d2
Armando Roman <armandoroman1016@gmail.com>
2019-10-09 14:49:49 -0700

    single farm styled

--- src/components/consumer/Farm.js
+++ src/components/consumer/Farm.js
@@ -6,8 +6,9 @@ const Farm = props => {
 
     return (
         <div className = 'farm-container'>
-            <p>{farm.name}</p>
-            <p>{farm.address}</p>
+            <img src='https://images.unsplash.com/photo-1553285906-9a0604da169f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=842&q=80' alt = 'farm_logo' className = 'logo'/>
+            <p className = 'farm-name'>{farm.name}</p>
+            <p className = 'farm-address'>{farm.address}</p>
         </div>
         )
 }
